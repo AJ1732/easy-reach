45733a32988116fb903786ec34ed309f0a07977f
AJ1732 <ejemeniboi@gmail.com>
2024-08-19 00:32:52 +0100

    style: add custom color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -12,8 +9,19 @@ export default {
         magnivera: ["Magnivera Trial", "sans-serif"],
         euclid: ["Euclid Circular A", "sans-serif"],
       },
+      colors: {
+        primary: {
+          500: "#00563B",
+          600: "#00543A",
+        },
+        secondary: {
+          500: "#D28E00",
+          op: "#FFB62742",
+        },
+        grey: "#585858",
+        dark: "#0C0C0C",
+      },
     },
   },
   plugins: [],
-}
-
+};
