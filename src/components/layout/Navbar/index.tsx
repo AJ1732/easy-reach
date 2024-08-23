d6bf13eb112bf4f0285784b8cab2042e79550263
AJ1732 <ejemeniboi@gmail.com>
2024-08-23 22:33:42 +0100

    style: update the navlink active state

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,6 +1,22 @@
 import { ButtonLink } from "@/components";
+import { cn } from "@/utils/cn";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const navlink = [
+    {
+      name: "home",
+      link: "/",
+    },
+    {
+      name: "about",
+      link: "/about",
+    },
+    {
+      name: "blog",
+      link: "/blog",
+    },
+  ];
   return (
     <header className="full-width content-grid fixed w-full py-8">
       <nav className="itce flex items-center justify-between">
@@ -10,12 +26,22 @@ const Navbar = () => {
 
         <div className="flex items-center justify-end gap-20">
           <ul className="flex items-center justify-center gap-[3.75rem] p-2.5">
-            {[1, 2, 3].map((item) => (
+            {navlink.map(({ name, link }) => (
               <li
-                key={item}
-                className="p-2.5 font-euclid text-sm font-semibold uppercase tracking-wide text-white"
+                key={name}
+                className="font-circularStd text-sm font-medium uppercase tracking-wide text-white"
               >
-                Link
+                <NavLink
+                  to={link}
+                  className={({ isActive }) =>
+                    cn(
+                      `relative flex items-center justify-center p-2.5 before:absolute before:bottom-0 before:size-[0.375rem] before:rounded-full`,
+                      isActive && "before:bg-white",
+                    )
+                  }
+                >
+                  {name}
+                </NavLink>
               </li>
             ))}
           </ul>
