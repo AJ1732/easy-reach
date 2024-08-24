027912de6c04eeaf047a48cd2024ec972f81bc54
AJ1732 <ejemeniboi@gmail.com>
2024-08-24 23:30:35 +0100

    chore: build fearure section

--- src/application/LandingPage/pages/HomePage/sections/FeatureSection/index.tsx
+++ src/application/LandingPage/pages/HomePage/sections/FeatureSection/index.tsx
@@ -1,4 +1,20 @@
+import { FeatureCard, SectionHeading } from "@/components";
+
 const FeatureSection = () => {
-  return <section>FeatureSection</section>;
+  return (
+    <section className="space-y-[5.45rem] py-[7.5rem]">
+      <SectionHeading
+        line
+        title="features"
+        before="before:content-['features']"
+      />
+
+      <div className="flex flex-wrap items-center justify-between gap-16">
+        {[1, 2, 3, 4].map((item) => (
+          <FeatureCard key={item} />
+        ))}
+      </div>
+    </section>
+  );
 };
 export default FeatureSection;
