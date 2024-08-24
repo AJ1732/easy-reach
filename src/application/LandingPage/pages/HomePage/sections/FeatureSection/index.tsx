0b737996449aacf22a85d3adad50c630f899263f
AJ1732 <ejemeniboi@gmail.com>
2024-08-25 00:04:15 +0100

    chore: add feature section data

--- src/application/LandingPage/pages/HomePage/sections/FeatureSection/index.tsx
+++ src/application/LandingPage/pages/HomePage/sections/FeatureSection/index.tsx
@@ -1,4 +1,5 @@
 import { FeatureCard, SectionHeading } from "@/components";
+import { FeatureData } from "@/data";
 
 const FeatureSection = () => {
   return (
@@ -9,9 +10,9 @@ const FeatureSection = () => {
         before="before:content-['features']"
       />
 
-      <div className="flex flex-wrap items-center justify-between gap-16">
-        {[1, 2, 3, 4].map((item) => (
-          <FeatureCard key={item} />
+      <div className="flex flex-wrap items-start justify-between gap-16">
+        {FeatureData.map(({ id, icon, title, content }) => (
+          <FeatureCard key={id} icon={icon} title={title} content={content} />
         ))}
       </div>
     </section>
