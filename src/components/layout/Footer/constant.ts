13b408e23ff6256b1793e88d7ab2c51f5689b5da
AJ1732 <ejemeniboi@gmail.com>
2024-08-26 17:58:41 +0100

    chore: build footer section

---
+++ src/components/layout/Footer/constant.ts
@@ -0,0 +1,74 @@
+export const footerLinks = [
+  {
+    heading: "quick links",
+    links: [
+      {
+        name: "about us",
+        link: "/about",
+      },
+      {
+        name: "blog",
+        link: "/blog",
+      },
+      {
+        name: "contact",
+        link: "/",
+      },
+    ],
+  },
+  {
+    heading: "contact us",
+    links: [
+      {
+        name: "overview",
+        link: "/",
+      },
+      {
+        name: "sitemap",
+        link: "/",
+      },
+      {
+        name: "events",
+        link: "/",
+      },
+    ],
+  },
+  {
+    heading: "features",
+    links: [
+      {
+        name: "data survey",
+        link: "/",
+      },
+      {
+        name: "resource",
+        link: "/",
+      },
+      {
+        name: "youth",
+        link: "/",
+      },
+    ],
+  },
+  {
+    heading: "socials",
+    links: [
+      {
+        name: "fa-brands fa-facebook",
+        link: "/",
+      },
+      {
+        name: "fa-brands fa-twitter",
+        link: "/",
+      },
+      {
+        name: "fa-brands fa-instagram",
+        link: "/",
+      },
+      {
+        name: "fa-brands fa-linkedin",
+        link: "/",
+      },
+    ],
+  },
+];
--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,7 +1,35 @@
+import { footerLinks } from "./constant";
+
 const Footer = () => {
   return (
     <footer className="full-width content-grid space-y-[6.25rem] bg-primary-500 py-[6.25rem] font-circularStd text-white">
-      <div>Footer Links</div>
+      <div className="flex flex-wrap items-start justify-between">
+        {footerLinks.map(({ heading, links }) => (
+          <div key={heading} className="space-y-5 font-circularStd">
+            <h3 className="text-lg font-black uppercase">{heading}</h3>
+
+            <ul className="space-y-5">
+              {links.map(({ name }) => {
+                if (heading === "socials") {
+                  return (
+                    <li key={name} className="mx-2 inline-block first:ml-0">
+                      <i className={`${name} text-3xl`}></i>
+                    </li>
+                  );
+                }
+                return (
+                  <li
+                    key={name}
+                    className="text-xl font-normal first-letter:capitalize"
+                  >
+                    {name}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ))}
+      </div>
 
       <hr />
 
