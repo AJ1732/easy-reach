1050f8501cc0ea42e7b6dbe324512372b2b076c1
AJ1732 <ejemeniboi@gmail.com>
2024-10-12 00:02:35 +0100

    chore: update responsiveness

--- src/application/LandingPage/pages/HomePage/sections/ContactSection/index.tsx
+++ src/application/LandingPage/pages/HomePage/sections/ContactSection/index.tsx
@@ -2,24 +2,24 @@ import { contactData } from "./constant";
 
 const ContactSection = () => {
   return (
-    <section className="pb-56 pt-40">
-      <div className="flex items-start justify-between">
-        <div className="max-w-[30rem] space-y-16 font-circularStd">
+    <section className="~pb-32/56 ~pt-20/40">
+      <div className="flex items-start justify-between ~gap-16/0 max-md:flex-col">
+        <div className="~space-y-8/16 max-w-[30rem] font-circularStd">
           {contactData.map(({ title, iconClass, text }) => (
             <div key={title} className="flex items-center justify-start gap-7">
-              <div className="flex min-w-[3.125rem] min-h-[3.125rem] items-center justify-center rounded-full bg-primary-500">
+              <div className="flex min-h-[3.125rem] min-w-[3.125rem] items-center justify-center rounded-full bg-primary-500">
                 <i className={iconClass}></i>
               </div>
 
-              <div className="space-y-3 text-black/80">
+              <div className="~space-y-0.5/3 text-black/80">
                 <h4 className="uppercase">{title}</h4>
-                <p className="text-2xl font-medium">{text}</p>
+                <p className="~text-lg/2xl font-medium">{text}</p>
               </div>
             </div>
           ))}
         </div>
 
-        <form className="min-w-[34.5rem] space-y-12">
+        <form className="w-full max-w-[34.5rem] space-y-12">
           <div className="flex flex-col gap-5">
             <label
               htmlFor="email"
